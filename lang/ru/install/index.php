b85d1554ebd7e0a374c3b7be02897c5d84e7c7f6
Denis Kulichkin <onexhovia@gmail.com>
2014-09-24 16:10:31 +0400

    Init repo

---
+++ lang/ru/install/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the Studio Fact package.
+ *
+ * (c) Kulichkin Denis (onEXHovia) <onexhovia@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$MESS['LOGGER_MODULE_NAME'] = 'Логирование';
+$MESS['LOGGER_MODULE_DESCRIPTION'] = 'Логирование данных';
+$MESS['PARTNER_NAME'] = 'ООО «Центр интернет технологий «ФАКТ»';
+$MESS['PARTNER_URI'] = 'http://www.studiofact.ru/';
