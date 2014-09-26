04a046fb03b44b85718df5079d63561eab5203c6
Denis Kulichkin <onexhovia@gmail.com>
2014-09-26 10:02:29 +0400

    Add menu

---
+++ admin/menu.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Studio Fact package.
+ *
+ * (c) Kulichkin Denis (onEXHovia) <onexhovia@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Loader;
+
+Loc::loadMessages(__FILE__);
+Loader::includeModule('citfact.logger');
+
+$menuList[] = array(
+    'parent_menu' => (Loader::includeModule('citfact.core')) ? 'global_menu_citfact' : 'global_menu_services',
+    'section' => 'logger',
+    'sort' => 200,
+    'text' => Loc::getMessage('LOGGER_TITLE'),
+    'url' => '',
+    'icon' => 'logger_menu_icon',
+    'page_icon' => 'logger_page_icon',
+    'more_url' => array(),
+    'items_id' => 'logger_menu',
+    'items' => array(),
+);
+
+return $menuList;
