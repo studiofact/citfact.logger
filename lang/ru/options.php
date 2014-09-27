98cb29b5899bf6e198b883bc07c5dbeaefce2bcf
Denis Kulichkin <onexhovia@gmail.com>
2014-09-27 14:18:59 +0400

    Add option

---
+++ lang/ru/options.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the Studio Fact package.
+ *
+ * (c) Kulichkin Denis (onEXHovia) <onexhovia@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$MESS['LOGGER_CLEAN_LOGS'] = 'Удалять старые логи';
+$MESS['LOGGER_STORAGE_TIME'] = 'Время хранения логов(в секундах)';
+$MESS['LOGGER_TAB'] = 'Настройки';
+$MESS['LOGGER_TAB_TITLE'] = 'Настройки';
+$MESS['BTN_SAVE'] = 'Сохранить';
+$MESS['BTN_RESTORE'] = 'Сбросить настройки';
--- options.php
+++ options.php
@@ -9,3 +9,73 @@
  * file that was distributed with this source code.
  */
 
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Application;
+use Bitrix\Main\Config;
+
+Loc::loadMessages(__FILE__);
+
+$app = Application::getInstance();
+$request = $app->getContext()->getRequest();
+$moduleId = 'citfact.logger';
+
+$allOptions = array(
+    array('CLEAN_LOGS', Loc::getMessage('LOGGER_CLEAN_LOGS'), 'Y', array('checkbox', 'Y')),
+    array('STORAGE_TIME', Loc::getMessage('LOGGER_STORAGE_TIME'), 604800, array('text', 20)),
+);
+
+$controlTabs = array(array(
+    'DIV' => 'tab-settings',
+    'TAB' => Loc::getMessage('LOGGER_TAB'),
+    'TITLE' => Loc::getMessage('LOGGER_TAB_TITLE'),
+));
+
+if ($request->isPost() && $request->getPost('RESTORE_DEFAULTS')) {
+    Config\Option::delete($moduleId);
+}
+
+if ($request->isPost() && $request->getPost('UPDATE')) {
+    foreach ($allOptions as $option) {
+        $value = $request->getPost($option[0]);
+        $value = ($option[3][0] == 'checkbox' && $value != 'Y') ? 'N' : $value;
+        Config\Option::set($moduleId, $option[0], $value);
+    }
+}
+
+$tabControl = new CAdminTabControl('control', $controlTabs);
+$tabControl->Begin();
+?>
+<form method="post" action="<?= $GLOBALS['APPLICATION']->GetCurPage() ?>">
+    <input type="hidden" name="mid" value="<?= $moduleId ?>">
+    <input type="hidden" name="lang" value="<?= LANGUAGE_ID ?>">
+    <? $tabControl->BeginNextTab(); ?>
+    <? foreach ($allOptions as $option): ?>
+        <?
+        $value = Config\Option::get($moduleId, $option[0], $option[2]);
+        $type = $option[3];
+        ?>
+        <tr>
+            <td width="40%" nowrap <?= ($type[0] == 'textarea') ? 'class="adm-detail-valign-top"' : '' ?>>
+                <label for="<?= htmlspecialcharsbx($option[0]) ?>"><?= $option[1] ?>:</label>
+            <td width="60%">
+                <? if ($type[0] == 'checkbox'): ?>
+                    <input type="checkbox" id="<?= htmlspecialcharsbx($option[0]) ?>"
+                           name="<?= htmlspecialcharsbx($option[0]) ?>"
+                           value="Y" <?= ($value == 'Y') ? 'checked' : '' ?>>
+                <? elseif ($type[0] == 'text'): ?>
+                    <input type="text" size="<?= $type[1] ?>" maxlength="255"
+                           value="<?= htmlspecialcharsbx($value) ?>"
+                           name="<?= htmlspecialcharsbx($option[0]) ?>">
+                <?
+                elseif ($type[0] == 'textarea'): ?>
+                    <textarea rows="<?= $type[1] ?>" cols="<?= $type[2] ?>"
+                              name="<? echo htmlspecialcharsbx($option[0]) ?>"><?= htmlspecialcharsbx($value) ?></textarea>
+                <?endif ?>
+            </td>
+        </tr>
+    <? endforeach; ?>
+    <? $tabControl->Buttons(); ?>
+    <input type="submit" name="UPDATE" class="adm-btn-save" value="<?= Loc::getMessage('BTN_SAVE') ?>">
+    <input type="submit" name="RESTORE_DEFAULTS" value="<?= Loc::getMessage("BTN_RESTORE") ?>">
+    <? $tabControl->End(); ?>
+</form>
